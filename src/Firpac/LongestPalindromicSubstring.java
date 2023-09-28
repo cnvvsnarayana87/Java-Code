20688b73bd120a196f863a639de2a5053831c4cb
V V Satyanarayana Nallam <cnvvsnarayana87@gmail.com>
2023-09-28 23:15:42 +0530

    Updated Java_Codes on 28-09-2023

---
+++ src/Firpac/LongestPalindromicSubstring.java
@@ -0,0 +1,44 @@
+// Program to Find the Longest Palindromic Substring
+package Firpac;
+
+public class LongestPalindromicSubstring {
+
+	public static void main(String[] args) {
+		String inputString = "xyxtcbabclmn";
+		String longestPalindrome = findLongestPalindrome(inputString);
+		System.out.println("Longest Palindromic Substring : " + longestPalindrome);
+	}
+
+	public static String findLongestPalindrome(String inputString) {
+		int[] stringCodePoint = inputString.codePoints().toArray();
+		StringBuilder palindromicSubstring = new StringBuilder();
+
+		int start = 0;
+		int end = 0;
+		for (int i = 1; i < stringCodePoint.length; i++) {
+			int j = 0;
+			while (i - j >= 0 && i + j < stringCodePoint.length && stringCodePoint[i - j] == stringCodePoint[i + j]) {
+				if (2 * j + 1 > end - start) {
+					start = i - j;
+					end = i + j;
+				}
+				j++;
+			}
+
+			j = 0;
+			while (i - j - 1 >= 0 && i + j < stringCodePoint.length
+					&& stringCodePoint[i - j - 1] == stringCodePoint[i + j]) {
+				if (2 * j + 2 > end - start) {
+					start = i - j - 1;
+					end = i + j;
+				}
+				j++;
+			}
+		}
+
+		for (int k = start; k <= end; k++) {
+			palindromicSubstring.appendCodePoint(stringCodePoint[k]);
+		}
+		return palindromicSubstring.toString();
+	}
+}
---
+++ src/Firpac/ZigZagCoversion.java
@@ -0,0 +1,11 @@
+// ZigZag Conversion Program in Java
+package Firpac;
+
+public class ZigZagCoversion {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
