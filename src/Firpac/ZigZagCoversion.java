d36fb0a54844ced34f30b011d8f9d1be88119278
V V Satyanarayana Nallam <cnvvsnarayana87@gmail.com>
2023-09-29 21:23:19 +0530

    Updated Java_Codes on 29-09-2023

--- src/Firpac/ZigZagCoversion.java
+++ src/Firpac/ZigZagCoversion.java
@@ -1,11 +1,35 @@
 // ZigZag Conversion Program in Java
 package Firpac;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ZigZagCoversion {
 
 	public static void main(String[] args) {
-		
+		String inputString = "PAYPALISHIRING";
+		System.out.println("Input String : " + inputString);
+		char[] charInputString = inputString.toCharArray();
+		StringBuilder wordOfRow = new StringBuilder();
+		StringBuilder resultString = new StringBuilder();
+		List<String> rowWordsList = new ArrayList<>();
+		int numOfRows = 3;
 
+		for (int i = 0; i < numOfRows; i++) {
+			wordOfRow.setLength(0);
+			int k = i + 1;
+			int j = i;
+			while (j < charInputString.length) {
+				wordOfRow.append(charInputString[j]);
+				j = j + numOfRows;
+			}
+			System.out.println("Row " + k + " : " + wordOfRow);
+			rowWordsList.add(wordOfRow.toString());
+		}
+
+		for (String string : rowWordsList) {
+			resultString.append(string);
+		}
+		System.out.println("Output after ZigZag conversion : " + resultString);
 	}
-
 }
---
+++ src/Firpac/ZigZagPrint.java
@@ -0,0 +1,17 @@
+package Firpac;
+
+public class ZigZagPrint {
+
+	public static void main(String[] args) {
+		String inputString = "*";
+		int charForRow = 6;
+		int numberOfRows = 6;
+		for(int i = 1; i <= numberOfRows; i++) {
+			for(int j = 1; j <= charForRow; j++) {
+				System.out.print(inputString+" ");	
+			}
+			System.out.println();
+		}
+	}
+}
+
